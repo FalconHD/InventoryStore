7a8f987d24935d02fea8ac915f6a92c1209ab7a9
Falcon <youssbak.2015@gmail.com>
2021-03-12 22:48:42 +0100

    redirecter

---
+++ Web/redirecter.js
@@ -0,0 +1,10 @@
+ 
+   
+window.onresize = ()=>{
+    if(window.innerWidth < 500){
+        console.log("alo");
+        window.location.href = "https://inventorymobile.netlify.app/"
+    }else if(window.innerWidth > 500){
+        window.location.href = "https://inventoryweb.netlify.app/"
+    }
+}
